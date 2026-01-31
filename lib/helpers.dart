9c20b6709ba14637a806d5275b6fa7f9fcf5331e
stefare90 <stefare@hotmail.it>
2026-01-31 13:30:38 +0100

    Update client to use cronet_http to align with Google networking standards and bypass bot detection.

--- lib/helpers.dart
+++ lib/helpers.dart
@@ -1,7 +1,7 @@
 Map<String, String> initializeHeaders(String visitorData) {
   Map<String, String> h = {
     "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.42",
+        "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Mobile Safari/537.36",
     'accept': '*/*',
     'accept-encoding': 'gzip, deflate',
     'content-type': 'application/json',
@@ -9,7 +9,7 @@ Map<String, String> initializeHeaders(String visitorData) {
     "Origin": "https://music.youtube.com",
     'cookie': 'CONSENT=YES+1',
     'Accept-Language': 'en',
-    'X-Goog-Visitor-Id':visitorData,
+    'X-Goog-Visitor-Id': visitorData,
   };
 
   return h;
